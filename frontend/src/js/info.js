8533a0ed3f7914ef96bcfc33e2add6428f8a728b
sinta <ramenrice555@gmail.com>
2025-10-19 16:36:50 +0900

    fix main.js
    main.jsにmarkdownファイルをhtmlに変換するコードを追加

--- frontend/src/js/info.js
+++ frontend/src/js/info.js
@@ -19,20 +19,19 @@ fetch("https://worldtimeapi.org/api/ip")
   })
   .then((myJson) => {
     time.innerText = myJson["utc_datetime"];
-  })
+  });
 
 async function callApi() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(res);
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  console.log(res);
 
-    fetch(res["url"])
-        .then(response => {
-            return response.json();
-        })
-        .then((myJson) => {
-            place.innerText = myJson[1]["username"];
-        })
+  fetch(res["url"])
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      place.innerText = myJson[1]["username"];
+    });
 }
-
 
 callApi();
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -2,6 +2,23 @@
 import '../css/style.css';
 import $ from 'jquery';
 import './header.js';
+import { marked } from "marked";
+
+const {
+  positionals
+} = Utils.parseArgs({
+  allowPositionals: true,
+});
+
+const filePath = positionals[0];
+fs.readFile(filePath, {encoding: "utf8"}).then(file => {
+  //Markdownファイルをhtml形式に変換する
+  const html = marked.parse(file);
+  console.log(html);
+}).catch(err => {
+  console.error(err.massage);
+  process.exit(1);
+});
 
 $(function () {
 });
